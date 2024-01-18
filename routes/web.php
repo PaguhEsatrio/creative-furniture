9052edb0a22f63bd783f2fae92e7680d95239bc0
Paguh Esatrio <97024224+PaguhEsatrio@users.noreply.github.com>
2024-01-18 09:55:33 +0700

    firts commit
    landing page not fix

---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/1', function () {
+    return view('home1');
+});
+
+Route::get('/', function () {
+    return view('home');
+});
+
